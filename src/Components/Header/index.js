325bdc174628a09864a7b0450e3e7cbb584dfc86
reyhang <reyhanguneyy@outlook.com>
2022-10-26 14:01:39 +0300

    First init.

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header =() => {
+    return(
+        <header style={{marginBottom:20}}>
+            <h1> Header </h1>
+            <button> Gece Modunu Değiştir </button>
+        </header>
+     )
+}
+
+export default Header;
